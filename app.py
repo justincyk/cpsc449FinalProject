fc1c1603e83bdc4d541fe3b3da8187eaeaf50951
justincyk <jcykim@csu.fullerton.edu>
2023-05-07 21:12:08 -0700

    created mongodb cluster and created user account for access

---
+++ app.py
@@ -0,0 +1,14 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://bookstoreAdmin:9QTngUHHhULpVK6V@cluster0.dm3qvxd.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
