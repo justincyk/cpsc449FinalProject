bd26f510328bd4db231140cde050048b1d8ee3c5
Nicholas Caro <nicholascaro@csu.fullerton.edu>
2023-05-12 01:02:56 -0700

    moved models to seperate file

--- main.py
+++ main.py
@@ -3,95 +3,14 @@ from fastapi import FastAPI, Body, HTTPException, status
 from fastapi.openapi.utils import get_openapi
 from fastapi.responses import Response, JSONResponse
 from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel, Field, ValidationError, validator
-from bson import ObjectId
-from typing import Optional, List
+from typing import List
 import motor.motor_asyncio
+from models import BookModel, UpdatedBookModel
 
 URL = "mongodb+srv://bookstoreAdmin:9QTngUHHhULpVK6V@cluster0.dm3qvxd.mongodb.net/?retryWrites=true&w=majority"
 app = FastAPI()
 client = motor.motor_asyncio.AsyncIOMotorClient(URL)
 db = client.bookstore
-
-
-# helper class to resolve ObjectId
-class PyObjectId(ObjectId):
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate
-
-    @classmethod
-    def validate(cls, v):
-        if not ObjectId.is_valid(v):
-            raise ValueError("Invalid objectid")
-        return ObjectId(v)
-
-    @classmethod
-    def __modify_schema__(cls, field_schema):
-        field_schema.update(type="string")
-
-
-def normalize(name: str) -> str:
-    return ' '.join((word.capitalize()) for word in name.split(' '))
-
-# Book Model
-
-
-class BookModel(BaseModel):
-    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
-    title: str = Field(...)
-    author: str = Field(...)
-    description: str = Field(max_length=300)
-    price: float = Field(
-        gt=0, description="The price must be greater than zero")
-    stock: int = Field(gt=0, description="The stock must be greater than zero")
-
-    # validators
-    @validator('author')
-    def author_validator(cls, value):
-        if ' ' not in value:
-            raise ValueError('Author must contain first and last name')
-        return value.title()
-
-    _normalize_title = validator('title', allow_reuse=True)(normalize)
-    _normalize_author = validator('author', allow_reuse=True)(normalize)
-    _normalize_description = validator(
-        'description', allow_reuse=True)(normalize)
-
-    class Config:
-        allow_population_by_field_name = True
-        arbitrary_types_allowed = True
-        json_encoders = {ObjectId: str}
-        schema_extra = {
-            "example": {
-                "title": "The Obscene Bird of Night",
-                "author": "José Donoso",
-                "description": "A Chiliean Classic",
-                "price": "16.00",
-                "stock": "210",
-            }
-        }
-
-
-class UpdatedBookModel(BaseModel):
-    title: Optional[str]
-    author: Optional[str]
-    description: Optional[str]
-    price: Optional[float]
-    stock: Optional[int]
-
-    class Config:
-        arbitrary_types_allowed = True
-        json_encoders = {ObjectId: str}
-        schema_extra = {
-            "example": {
-                "title": "The Obscene Bird of Night",
-                "author": "José Donoso",
-                "description": "A Chiliean Classic",
-                "price": "16.00",
-                "stock": "210",
-            }
-        }
 
 
 @app.get("/")
